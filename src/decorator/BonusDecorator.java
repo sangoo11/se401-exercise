6f779fdb24baddd52737f14234a3d622324d2458
ht1505000 <22521439@gm.uit.edu.vn>
2025-10-18 19:20:28 +0700

    Add EmployeeDecorator and BonusDecorator classes to implement salary decoration functionality

---
+++ src/decorator/BonusDecorator.java
@@ -0,0 +1,23 @@
+package decorator;
+
+import composite.Employee;
+
+public class BonusDecorator extends EmployeeDecorator {
+    private double bonus;
+
+    public BonusDecorator(Employee decoratedEmployee, double bonus) {
+        super(decoratedEmployee);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double getSalary() {
+        return decoratedEmployee.getSalary() + bonus;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("🏅 Bonus applied to " + name + ": +" + bonus);
+        System.out.println("→ Total salary (with bonus): " + getSalary());
+    }
+}
---
+++ src/decorator/EmployeeDecorator.java
@@ -0,0 +1,22 @@
+package decorator;
+
+import composite.Employee;
+
+public abstract class EmployeeDecorator extends Employee {
+    protected Employee decoratedEmployee;
+
+    public EmployeeDecorator(Employee decoratedEmployee) {
+        super(decoratedEmployee.getName(), decoratedEmployee.getBaseSalary());
+        this.decoratedEmployee = decoratedEmployee;
+    }
+
+    @Override
+    public double getSalary() {
+        return decoratedEmployee.getSalary();
+    }
+
+    @Override
+    public void showInfo() {
+        decoratedEmployee.showInfo();
+    }
+}
