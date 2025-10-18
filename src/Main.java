e8a5aa1db41b4bb102546e18ae5aae85f732b094
Ngo Tran Quang Sang <112809865+sangoo11@users.noreply.github.com>
2025-10-18 16:32:35 +0700

    Update Builder
    Update design pattern Builder

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,21 @@
-import composite.Employee;
+import builder.CompanyBuilder;
 import composite.Manager;
 import composite.Staff;
 
 class Main {
     public static void main(String[] args) {
-        Manager ceo = new Manager("Ceo", 10000);
+        Manager company = new CompanyBuilder("Ceo", 10000)
+            .addDepartment("VDr Marketing", 5000, 
+                new Staff("NV Quang Cao", 2000))
+            .addDepartment("VDr San Xuat", 6000,
+                new Staff("NV Ke Toan", 2500),
+                new Staff("NV Van Chuyen", 2300))
+            .build();
 
-        Manager marketing = new Manager("VDr Marketing", 5000);
-        Manager sanxuat = new Manager("VDr San Xuat", 6000);
-
-        Staff qc = new Staff("NV Quang Cao", 2000);
-        Staff kt = new Staff("NV Ke Toan", 2500);
-        Staff vc = new Staff("NV Van Chuyen", 2300);
-
-        marketing.add(qc);
-
-        sanxuat.add(kt);
-        sanxuat.add(vc);
-
-        ceo.add(marketing);
-        ceo.add(sanxuat);
-
-        System.out.println("Total Salary: " + ceo.getSalary());
+        System.out.println("Total Salary: " + company.getSalary());
 
         System.out.println("###############################");
 
-        ceo.getInfo();
+        company.getInfo();
     }
 }
---
+++ src/builder/CompanyBuilder.java
@@ -0,0 +1,25 @@
+package builder;
+
+import composite.Employee;
+import composite.Manager;
+
+public class CompanyBuilder {
+    private Manager ceo;
+
+    public CompanyBuilder(String ceoName, double ceoSalary) {
+        ceo = new Manager(ceoName, ceoSalary);
+    }
+
+    public CompanyBuilder addDepartment(String deptName, double salary, Employee... employees) {
+        Manager dept = new Manager(deptName, salary);
+        for (Employee e : employees) {
+            dept.add(e);
+        }
+        ceo.add(dept);
+        return this;
+    }
+
+    public Manager build() {
+        return ceo;
+    }
+}
